baeb6be738affa68b7481386e0bd231601f4b460
Cuong Dang <cuong.developer19@gmail.com>
2020-12-03 10:50:03 +0700

    Setup Redux.

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
+import store from './app/store';
 import AppNavigator from './app/navigation/AppNavigator';
 
 const App = () => {
-  return <AppNavigator />;
+  return (
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
+  );
 };
 
 export default App;
---
+++ app/store/index.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import rootReducer from './reducers';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware)),
+);
+
+export default store;
