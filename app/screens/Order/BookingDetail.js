d333b3f33bbdcc63129858529e21f218ff86a935
hungcuuu <ngochihung2471999@gmail.com>
2020-12-30 17:21:06 +0700

    Design UI

--- app/screens/Order/BookingDetail.js
+++ app/screens/Order/BookingDetail.js
@@ -36,18 +36,59 @@ const BookingDetail = ({ navigation, route }) => {
     const renderParts = part => {
         return (
             <>
-                <View>
-                    <Text>{`${part.partName} ${part.priceEach} x ${part.quantity}  `}</Text>
-                    <Text></Text>
+                <View
+                    style={{
+                        flex: 1,
+                        // borderWidth: 1,
+                        marginHorizontal: 8,
+                        marginVertical: 4,
+                        flexDirection: 'row',
+                        // justifyContent: 'space-between',
+                    }}>
+                    <View
+                        style={{
+                            // borderWidth: 1,
+                            minHeight: 70,
+                            width: '20%',
+                        }}>
+                        <Image
+                            resizeMethod="resize"
+                            resizeMode="contain"
+                            source={{
+                                uri:
+                                    part.partImageUrls[0] ??
+                                    'https://i.vimeocdn.com/portrait/58832_300x300.jpg',
+                                height: '100%',
+                                width: '100%',
+                            }}
+                            style={{ width: '100%', height: '100%' }}
+                        />
+                    </View>
+
+                    <View style={{ width: '80%' }}>
+                        <Text>{` ${part.partName}   `}</Text>
+                        <Text
+                            style={{
+                                width: '100%',
+                                textAlign: 'right',
+                            }}>{`x ${part.quantity}`}</Text>
+                        <Text>{` ${formatMoney(part.priceEach)}`}</Text>
+                    </View>
                 </View>
             </>
         );
     };
     const renderServices = service => {
         return (
+            // ${formatMoney(service.price)}
             <>
-                <View style={{ borderWidth: 1 }}>
-                    <Text>{`${service.serviceName} ${service.price}`}</Text>
+                <View style={{}}>
+                    <Text
+                        style={{
+                            color: 'red',
+                            fontSize: 18,
+                            fontWeight: 'bold',
+                        }}>{`${service.serviceName} `}</Text>
                     <FlatList
                         data={service.parts ?? []}
                         keyExtractor={(item, index) => index.toString()}
@@ -58,7 +99,7 @@ const BookingDetail = ({ navigation, route }) => {
         );
     };
     const cancelBooking = id => {
-        return axios.delete('requests/' + id).then(res =>
+        axios.delete('requests/' + id).then(res =>
             // setCurrentBooking(res.data)
             {
                 if (res.status == 200) {
@@ -95,76 +136,125 @@ const BookingDetail = ({ navigation, route }) => {
     }, []);
     useEffect(() => {
         navigation.setOptions({ headerTitle: `request #${detail.id}` });
-        console.log(detail);
+        // console.log(detail.services[0].parts);
     }, []);
     return (
-        <ScrollView style={styles.container}>
-            <View>
-                <Text>{`${detail.provider.providerName}`}</Text>
-                <Text>{`Address: ${detail.provider.address}`}</Text>
-            </View>
-            <Card.Divider />
-            <View>
-                <Text>{`Booking Date: ${moment
-                    .unix(detail.bookingTime)
-                    .format('DD/MM/YYYY')}`}</Text>
-            </View>
-            <Card.Divider />
+        <View style={styles.container}>
+            <FlatList
+                ListHeaderComponent={
+                    <>
+                        <View style={{ alignSelf: 'center' }}>
+                            <Text
+                                style={{
+                                    fontSize: 15,
+                                    fontWeight: 'bold',
+                                }}>{`${detail.provider.providerName}`}</Text>
+                        </View>
 
-            <View>
-                <Text>
-                    <Text
-                        style={{
-                            backgroundColor: getStatusTagColor(detail.status),
-                            opacity: 0.7,
-                            paddingVertical: 8,
-                        }}>
-                        {`${detail.status}`}
-                    </Text>
-                </Text>
-            </View>
-            <Card.Divider />
+                        <View>
+                            {/* <Text>Address:</Text> */}
+                            <Text>{`${detail.provider.address}`}</Text>
+                        </View>
+                        <Card.Divider />
+                        <View style={styles.rowInfo}>
+                            <Text>Booking Date:</Text>
+                            <Text>{` ${moment
+                                .unix(detail.bookingTime)
+                                .format('DD/MM/YYYY')}`}</Text>
+                        </View>
+                        <Card.Divider />
 
-            <View>
-                <Text>{`${detail.userVehicle?.modelName ?? ''} ${detail.userVehicle
-                    ?.modelYear ?? ''}`}</Text>
-                <Text>{`PlateNumber ${detail.userVehicle?.plateNumber ?? ''}`}</Text>
-            </View>
+                        <View style={styles.rowInfo}>
+                            <Text>Status</Text>
+                            <Text>
+                                <Text
+                                    style={{
+                                        backgroundColor: getStatusTagColor(detail.status),
+                                        opacity: 0.7,
+                                        paddingVertical: 8,
+                                    }}>
+                                    {`${detail.status}`}
+                                </Text>
+                            </Text>
+                        </View>
+                        <Card.Divider />
 
-            <Card.Divider />
-            <View style={{ borderWidth: 1 }}>
-                <Text>{`Technician: ${detail.technician?.fullname ?? 'none'}`}</Text>
-                <View
-                    style={{
-                        borderWidth: 1,
-                        height: 200,
-                        width: '50%',
-                    }}>
-                    <Image
-                        resizeMethod="resize"
-                        resizeMode="cover"
-                        source={{
-                            uri:
-                                detail.technician?.imageUrl ??
-                                'https://i.vimeocdn.com/portrait/58832_300x300.jpg',
-                            height: '100%',
-                            width: '100%',
-                        }}
-                        style={{ width: '100%', height: '100%' }}
-                    />
-                </View>
-            </View>
-            <View style={{ borderWidth: 1 }}>
-                <FlatList
-                    data={detail.services ?? []}
-                    keyExtractor={item => item.id.toString()}
-                    renderItem={({ item: service }) => renderServices(service)}
-                />
-            </View>
-            <View style={{ height: 100 }}>
-                <Text>Total: {formatMoney(calculateRequestPrice(detail).total)}</Text>
-            </View>
-        </ScrollView>
+                        <View>
+                            <View style={styles.rowInfo}>
+                                <Text>Model:</Text>
+                                <Text>{`${detail.userVehicle?.modelName ?? ''} ${detail
+                                    .userVehicle?.modelYear ?? ''}`}</Text>
+                            </View>
+                            <View style={styles.rowInfo}>
+                                <Text>PlateNumber:</Text>
+                                <Text>{` ${detail.userVehicle?.plateNumber ?? ''}`}</Text>
+                            </View>
+                        </View>
+
+                        <Card.Divider />
+                        <View style={styles.rowInfo}>
+                            <Text>Technician:</Text>
+                            <Text>{` ${detail.technician?.fullname ?? 'none'}`}</Text>
+                            {/* <View
+                                style={{
+                                    borderWidth: 1,
+                                    height: 100,
+                                    width: 100,
+                                }}>
+                                <Image
+                                    resizeMethod="resize"
+                                    resizeMode="cover"
+                                    source={{
+                                        uri:
+                                            detail.technician?.imageUrl ??
+                                            'https://i.vimeocdn.com/portrait/58832_300x300.jpg',
+                                        height: '100%',
+                                        width: '100%',
+                                    }}
+                                    style={{ width: '100%', height: '100%' }}
+                                />
+                            </View> */}
+                        </View>
+                    </>
+                }
+                ListFooterComponent={
+                    <>
+                        <View
+                            style={{
+                                marginVertical: 8,
+                                borderTopWidth: 1,
+                            }}>
+                            <Text
+                                style={{
+                                    fontWeight: 'bold',
+                                    alignSelf: 'flex-end',
+                                }}>{`Service: ${formatMoney(
+                                calculateRequestPrice(detail).services,
+                            )}`}</Text>
+                            <Text
+                                style={{
+                                    fontWeight: 'bold',
+                                    alignSelf: 'flex-end',
+                                }}>{`Parts Price: ${formatMoney(
+                                calculateRequestPrice(detail).partsPrice,
+                            )}`}</Text>
+                            <Text
+                                style={{
+                                    fontWeight: 'bold',
+                                    alignSelf: 'flex-end',
+                                }}>{`Total: ${formatMoney(
+                                calculateRequestPrice(detail).total,
+                            )}`}</Text>
+                        </View>
+                    </>
+                }
+                data={detail.services ?? []}
+                keyExtractor={item => item.id.toString()}
+                renderItem={({ item: service }) => renderServices(service)}
+                initialNumToRender={3}
+                showsVerticalScrollIndicator={false}
+            />
+        </View>
     );
 };
 
@@ -173,8 +263,15 @@ export default BookingDetail;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        width: '100%',
         paddingVertical: 16,
         paddingHorizontal: 8,
+    },
+    rowInfo: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        margin: 8,
+        marginHorizontal: 10,
+        paddingHorizontal: 8,
+        // alignItems: 'center',
     },
 });
