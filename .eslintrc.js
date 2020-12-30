ad1db39c51c9723f54666c94c7a91125d4f57a60
Cuong Dang <cuong.developer19@gmail.com>
2020-12-31 00:54:08 +0700

    ( IMPORTANT ) Fixed ESLint to warn errors.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,4 @@
 module.exports = {
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  parser: 'babel-eslint',
-  plugins: ['react', 'react-native'],
-  rules: {
-    'react-native/no-unused-styles': 2,
-    'react-native/split-platform-components': 2,
-    'react-native/no-raw-text': 2,
-    'react-native/no-single-element-style-arrays': 2,
-  },
+    root: true,
+    extends: '@react-native-community',
 };
