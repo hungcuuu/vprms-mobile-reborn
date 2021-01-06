0e1d6e196d2bc837d07acc94bcffacfcd5c5ba38
Cuong Dang <cuong.developer19@gmail.com>
2021-01-06 16:26:37 +0700

    Trigger notification.

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import messaging from '@react-native-firebase/messaging';
+import { Alert } from 'react-native';
 
 import { store, persistor } from './app/store';
 import AppNavigator from './app/navigation/AppNavigator';
 
 const App = () => {
+    useEffect(() => {
+        const unsubscribe = messaging().onMessage(async remoteMessage => {
+            Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+        });
+
+        return unsubscribe;
+    }, []);
     return (
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
